2611de84b00bed249786bc6c43ca1783ba0bfe2d
Rish4bh0 <122444548+Rish4bh0@users.noreply.github.com>
2024-06-15 23:00:15 +0545

    Fixed project image not showing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
           <HeroSection />
         </div>
       </div>
-      <div className="max-w-7xl mx-auto p-5">
+      <div className="max-w-7xl mx-auto p-4">
         <Skills />
         <Projects />
         <div className="border-t mt-10">
